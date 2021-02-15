69a4f78e89c0efe95c02af0eab78f106a5df8c3c
vpuzin <vlad.puzin@gmail.com>
2021-02-15 23:30:12 +0300

    refactoring

--- Task02/init.py
+++ Task02/init.py
@@ -1,8 +1,6 @@
 EOL = '\n'
 RESULT_FILE_PATH = 'db_init.sql'
 
-USERS_TO_INSERT_FILE = 'users.txt'
-
 create_tables_queries = {
     "movies" : 'CREATE TABLE IF NOT EXISTS movies(id INTEGER PRIMARY KEY, title TEXT, year INTEGER, genres TEXT);' + EOL,
     "ratings" : 'CREATE TABLE IF NOT EXISTS ratings(id INTEGER PRIMARY KEY, user_id INTEGER, movie_id INTEGER, rating REAL, timestamp INTEGER);' + EOL,
@@ -10,11 +8,18 @@ create_tables_queries = {
     "users" : 'CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT, email TEXT, gender TEXT, register_date TEXT, occupation TEXT);' + EOL
 }
 
-purge_tables_quiries = {
+purge_tables_queries = {
     "movies" : 'DROP TABLE IF EXISTS movies;' + EOL,
     "ratings" : 'DROP TABLE IF EXISTS ratings;' + EOL,
     "tags" : 'DROP TABLE IF EXISTS tags;' + EOL,
     "users" : 'DROP TABLE IF EXISTS users;' + EOL
+}
+
+fill_database_core_queries = {
+    "movies" : 'INSERT INTO movies (title, year, genres) VALUES ',
+    "ratings" : 'INSERT INTO ratings (user_id, movie_id, rating, timestamp) VALUES ',
+    "tags" : 'INSERT INTO tags (user_id, movie_id, tag, timestamp) VALUES ',
+    "users" : 'INSERT INTO users (name, email, gender, register_date, occupation) VALUES '
 }
 
 def write_create_tables_queries(file):
@@ -24,44 +29,59 @@ def write_create_tables_queries(file):
     file.write(create_tables_queries['users'])
 
 def write_purge_database_queries(file):
-    file.write(purge_tables_quiries['movies'])
-    file.write(purge_tables_quiries['ratings'])
-    file.write(purge_tables_quiries['tags'])
-    file.write(purge_tables_quiries['users'])
+    file.write(purge_tables_queries['movies'])
+    file.write(purge_tables_queries['ratings'])
+    file.write(purge_tables_queries['tags'])
+    file.write(purge_tables_queries['users'])
 
-def read_txt_file():
-    usersFile = open(USERS_TO_INSERT_FILE, 'r')
-    sqlCoreInsertQuery = 'INSERT INTO users (id, name, email, gender, register_date, occupation) VALUES '
+def compose_insert_query_from_file(coreInsertQuery, file_name, splitter):
+    usersFile = open(file_name, 'r')
+    sqlCoreInsertQuery = coreInsertQuery
+
+    lines = None
+    if file_name[-3:] == 'csv':
+        lines = usersFile.readlines()[1:]
+    else:
+        lines = usersFile.readlines()
+
     comma = ','
 
-    i = 0
-    for rawLine in usersFile.readlines():
-        fields = rawLine.split('|')
-        
+    for rawLine in lines:
+        fields = rawLine.split(splitter)
+
+        if file_name[-3:] == 'csv':
+            fields.pop(0)
+
+        fields[:] = ['"' + x + '"' for x in fields]
+
         commaSeparatedFields = comma.join(fields)
-        fieldsToInsertSqlQuery = '(' + commaSeparatedFields +')'
+        fieldsToInsertSqlQuery = '(' + commaSeparatedFields + ')'
         sqlCoreInsertQuery += fieldsToInsertSqlQuery + ','
-        if i == 3:
-            break
-        i = i + 1
 
-    return sqlCoreInsertQuery
+    return sqlCoreInsertQuery[:-1] + ';'
 
-def echo(line):
-    print(line + EOL)
+def write(file, line):
+    file.write(line)
 
 def handle():
     file = open(RESULT_FILE_PATH, 'a')
 
-    sqlCoreInsertQuery = read_txt_file()
-    print(sqlCoreInsertQuery)
-    #echo('Dropping existing tables')
-    #write_purge_database_queries(file)
+    write_purge_database_queries(file)
 
-    #echo('Create tables')
-    #write_create_tables_queries(file)
+    write_create_tables_queries(file)
 
-    #echo('All done')
+    insertUsersQuery = compose_insert_query_from_file(fill_database_core_queries['users'], 'users.txt', '|')
+
+    insertMoviesQuery = compose_insert_query_from_file(fill_database_core_queries['movies'], 'movies.csv', ',')
+
+    insertRatingsQuery = compose_insert_query_from_file(fill_database_core_queries['ratings'], 'ratings.csv', ',')
+
+    insertTagsQuery = compose_insert_query_from_file(fill_database_core_queries['tags'], 'tags.csv', ',')
+
+    write(file, insertUsersQuery)
+    write(file, insertMoviesQuery)
+    write(file, insertRatingsQuery)
+    write(file, insertTagsQuery)
 
     file.close()
 
